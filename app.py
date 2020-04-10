eb50d2e8819908325fea8eccbb683724073926b6
Eric <eric.l.hadley@protonmail.com>
2020-04-10 14:32:04 -0400

    added comments to code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask 
 from flask import request
 
+#declare flask object and route index page
 app = Flask(__name__)
 @app.route('/')
 def index():
 
+    #get the user agent of the client
     user_agent = request.headers.get('User-Agent')
 
     #variables for architecture test     
@@ -25,6 +27,7 @@ def index():
     operating_systems = ["android","linux","windows"]
     operating_system = ""
 
+    #operating system test against user agent
     if linux_test in user_agent:
         operating_system = operating_systems[1]
         if android_test in user_agent:
@@ -32,5 +35,26 @@ def index():
     else:
         operating_system = operating_systems[2]
 
-    arch_os_pattern = architecture + " " + operating_system    
-    return arch_os_pattern
+    #concatenated pattern for switch
+    arch_os_pattern = architecture + " " + operating_system
+
+    #set filepath with switch string pattern
+    switch(arch_os_pattern) {
+        case "x64 linux" : filepath = ""
+            break
+        case "x64 windows" : filepath = ""
+            break
+        case "x64 android": filepath = ""
+            break
+        case "x86 linux" : filepath = ""
+            break
+        case "x86 windows" : filepath = ""
+            break
+        case "x86 android" : filepath = ""
+            break
+        default:    filepath = ""
+            break
+    }   
+
+    #send set payload
+    return send_file(filepath)
