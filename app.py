0117b660a9ce7409d090f10093d4375684d18802
Eric <eric.l.hadley@protonmail.com>
2020-04-10 14:08:49 -0400

    added index page

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 from flask import Flask 
+from flask import request
 
-app = Flask(__name__)
+user_agent = request.headers.get('User-Agent')
 
-@app.route('/')
-def index():
-    return ':)'
+#variables for architecture test     
+x64_test = "x86_64"
+architectures = ["x64","x86"]
+architecture = ""
+
+#architecture test against user agent
+if x64_test in user_agent:
+    architecture = architectures[0]
+else:
+    architecture = architectures[1]
+
+#variables for operating systems test
+android_test = "Android"
+linux_test = "Linux"
+window_test = "Windows"
+operating_systems = ["android","linux","windows"]
+operating_system = ""
+
+if linux_test in user_agent:
+    operating_system = operating_systems[1]
+    if android_test in user_agent:
+        operating_system = operating_systems[0]
+else:
+    operating_system = operating_systems[2]
+    
